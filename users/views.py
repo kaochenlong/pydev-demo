36c8a3ebddc3fd9eab02c8fca687379fc5ccc21c
Eddie Kao <eddie@5xcampus.com>
2024-11-28 11:47:35 +0800

    add user login

--- users/views.py
+++ users/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as login_user
+from django.contrib import messages
 
 
 def login(request):
+    if request.POST:
+        # 驗證
+        user = authenticate(
+            username=request.POST.get("username"),
+            password=request.POST.get("password"),
+        )
+
+        if user is not None:
+            login_user(request, user)
+            messages.success(request, "登入成功")
+            return redirect("pages:home")
+        else:
+            messages.success(request, "登入失敗")
+            return redirect("users:login")
+
     return render(request, "users/login.html")
 
 
 def register(request):
     if request.POST:
         # 註冊
-        # redirect 首頁
-        pass
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "註冊成功")
+            return redirect("pages:home")
+        else:
+            return HttpResponse(form.error_messages)
+
     return render(request, "users/register.html")
 
 

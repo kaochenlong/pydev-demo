ca0b32f454bb33712310991796816ad31ee24b43
Eddie Kao <eddie@5xcampus.com>
2024-11-28 12:54:19 +0800

    add alpine

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,1 +1,11 @@
 import "htmx.org";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+Alpine.start();
+
+Alpine.data("comment_form", () => ({
+  init() {
+    console.log(123);
+  },
+}));
